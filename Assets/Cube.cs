8e0b386507b1a24186898c60b6ab1ce6fa19680c
Samuwhale <sfspithorst@gmail.com>
2023-03-31 10:29:29 +0200

    work on spawner

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -13,12 +13,28 @@ public class Cube : MonoBehaviour
     private Vector3 _fromLocation;
     private Vector3 _toLocation;
     private float _moveTimer = 0;
-    private float _lerpProgress; 
-    
+    private float _lerpProgress;
+    private Material _material;
+    private bool _shouldMove;
+
+    private void Awake()
+    {
+        _material = GetComponent<Renderer>().material;
+    }
+
     private void Start()
     {
-        SetHeadings();
-        Move();
+        MS.Main.GameManager.OnGameStateChanged += GameManager_OnGameStateChanged;
+    }
+
+    private void GameManager_OnGameStateChanged(GameManager.States state)
+    {
+        if (state != GameManager.States.Playing)
+        {
+            StopMoving();
+            return;
+        }
+        
     }
 
     private void Update()
@@ -26,23 +42,28 @@ public class Cube : MonoBehaviour
         Move();
     }
 
+    public void SetColor(Color color)
+    {
+        _material.color = color;
+    }
+
     void Move()
     {
+        if (!_shouldMove) return;
+
         _moveTimer += Time.deltaTime;
         _lerpProgress = _moveTimer / _moveCompletionTime;
-        
+
         transform.position = Vector3.Lerp(_fromLocation, _toLocation, Mathf.SmoothStep(0, 1, _lerpProgress));
 
         if (_moveTimer > _moveCompletionTime)
         {
             SwitchHeading();
         }
-        
-        Debug.Log($"moveTimer = {_moveTimer}, lerpprog = {_lerpProgress}");
     }
 
 
-    void SetHeadings()
+    public void SetHeadings()
     {
         _fromLocation = transform.position + transform.right * (_moveDistance / 2);
         _toLocation = transform.position + -transform.right * (_moveDistance / 2);
@@ -52,5 +73,20 @@ public class Cube : MonoBehaviour
     {
         _moveTimer = 0;
         (_toLocation, _fromLocation) = (_fromLocation, _toLocation);
+    }
+
+    public void StopMoving()
+    {
+        _shouldMove = false;
+    }
+
+    public void StartMoving()
+    {
+        _shouldMove = true;
+    }
+
+    void SplitOver(Transform cubeBelow)
+    {
+        
     }
 }
